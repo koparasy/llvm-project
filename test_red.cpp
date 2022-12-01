7b03026d52f70d3b4b35110fa842f6d4e371258e
Konstantinos Parasyris <parasyris1@llnl.gov>
2022-11-30 21:33:10 -0800

    Test case

---
+++ test_red.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+#define DTYPE float
+
+int main (int argc, char *argv[])
+{
+  long Elements = std::atol(argv[1]) * 1024L;
+  DTYPE  *Data = new DTYPE[Elements];
+
+#pragma omp target teams distribute parallel for map(tofrom:Data[:Elements])
+  for (long I = 0 ; I < Elements; I++)
+    Data[I] = I - (Elements / 2);
+
+  DTYPE Sum = 0;
+#pragma omp target teams distribute parallel for map(tofrom:Data[:Elements]) reduction(+:Sum)
+  for (long I = 0 ; I < Elements; I++)
+    Data[I] = I - (Elements / 2);
+
+  std::cout<< "Computed :" << Sum << "Correct: " << (-Elements/2) << "\n";
+
+  delete [] Data;
+  return 0;
+}
